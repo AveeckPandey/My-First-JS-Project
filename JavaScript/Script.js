72989d687f0bfca1f4be002b49cf166dfda386a8
AveeckPandey <aveeckkumarpandey@gmail.com>
2024-08-04 17:31:00 +0530

    Add files via upload

---
+++ JavaScript/Script.js
@@ -0,0 +1,9 @@
+var a = document.querySelector("#add")
+var b = document.querySelector("h5")
+a.addEventListener("click",function(){
+    b.innerHTML = "Friends"
+    b.style.color = "green"
+    
+
+     
+})
